a4ea9ce4ad1f8731249938330225cb3349374853
William <william.gates.3299@gmail.com>
2024-09-11 17:26:35 +0330

    Improved Error Message

--- app/src/main/java/com/xeniac/youtubemusicplayer/feature_youtube_player/presentation/components/ErrorMessage.kt
+++ app/src/main/java/com/xeniac/youtubemusicplayer/feature_youtube_player/presentation/components/ErrorMessage.kt
@@ -5,30 +5,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.xeniac.youtubemusicplayer.R
 
 @Composable
 fun ErrorMessage(
     message: String?,
     modifier: Modifier = Modifier,
+    errorMessageText: String = stringResource(
+        id = R.string.youtube_player_error_message,
+        message ?: ""
+    ),
     fontSize: TextUnit = 24.sp,
     lineHeight: TextUnit = 24.sp,
     fontWeight: FontWeight = FontWeight.Bold,
     textAlign: TextAlign = TextAlign.Center,
     color: Color = MaterialTheme.colorScheme.error
 ) {
-    message?.let {
-        Text(
-            text = message,
-            fontSize = fontSize,
-            lineHeight = lineHeight,
-            fontWeight = fontWeight,
-            textAlign = textAlign,
-            color = color,
-            modifier = modifier
-        )
-    }
+    Text(
+        text = errorMessageText,
+        fontSize = fontSize,
+        lineHeight = lineHeight,
+        fontWeight = fontWeight,
+        textAlign = textAlign,
+        color = color,
+        modifier = modifier
+    )
 }
