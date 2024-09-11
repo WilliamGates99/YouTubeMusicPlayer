0f221cb670b079b7b2c5d30a54a2b008e84e1995
William <william.gates.3299@gmail.com>
2024-09-11 20:20:51 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/youtubemusicplayer/feature_youtube_player/presentation/components/YoutubePlayerAndroidView.kt
+++ app/src/main/java/com/xeniac/youtubemusicplayer/feature_youtube_player/presentation/components/YoutubePlayerAndroidView.kt
@@ -60,6 +60,9 @@ fun YoutubePlayerAndroidView(
                 })
             }
         },
+        onRelease = { playerView ->
+            playerView.release()
+        },
         modifier = modifier
     )
 }
